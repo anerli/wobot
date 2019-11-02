22f8c19d505b148092742cc429150f4d43477623
flarphengarg <anders.erik.lie@gmail.com>
2019-11-02 10:24:11 -0500

    send messages to jason

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from twilio import twiml
+from twilio_tools import send_message
 
 app = Flask(__name__)
 
@@ -9,11 +9,18 @@ db = SQLAlchemy(app)
 
 class User_db (db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    # string of 1's and 0's in a particular order to denote which muscle groups have been used
+    # '10' is number muscle groups
+    last_worked_mgroups = db.Column(db.String(10))
     
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        msg = request.form['msg']
+        num = request.form['num']
+        send_message(msg, num)
         return redirect('/')
     else:
         return render_template('index.html')
