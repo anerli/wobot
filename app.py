c0377733118c03274e3ceb6c4225c3a5c6b919ab
flarphengarg <anders.erik.lie@gmail.com>
2019-11-02 12:44:55 -0500

    basic ui

--- app.py
+++ app.py
@@ -1,29 +1,49 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from twilio_tools import send_message
+from flask_apscheduler import APScheduler
+import schedule_tools
+import time
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+scheduler = APScheduler()
+scheduler.init_app(app)
+
 
 class User_db (db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     # string of 1's and 0's in a particular order to denote which muscle groups have been used
     # '10' is number muscle groups
-    last_worked_mgroups = db.Column(db.String(10))
+    last_worked_mgroups = db.Column(db.String(10), default='1111100000')
     
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        msg = request.form['msg']
-        num = request.form['num']
-        send_message(msg, num)
+        #msg = request.form['msg']
+        #num = request.form['num']
+        #send_message(msg, num)
+
+        phone_number = request.form['phone_number']
+        # etc...
+        
         return redirect('/')
     else:
         return render_template('index.html')
 
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
 if __name__ == '__main__':
+    #schedule_tools.schedule_the_message_send_scheduler(scheduler)
+    #scheduler.add_job(func=print_date_time, trigger="interval", seconds=3, id='dateprinter')
+    scheduler.add_job(func=lambda: schedule_tools.schedule_message_sends(scheduler, ), trigger="cron", hour=0, id='dateprinter')
+    scheduler.start()
+
+    # Remember to take off debug mode on upload. This also fixes sheduler running things twice
     app.run(debug = True)
+    
---
+++ schedule_tools.py
@@ -0,0 +1,23 @@
+'''
+Run once when server up
+'''
+def schedule_the_message_send_scheduler(scheduler):
+    pass
+
+
+'''
+Need to schedule this for every day, and w user setting call every time user changes or adds a settings
+
+Run on new usersetting whenever one is added or changed
+Run on all usersettings every day
+
+Takes in the db's us
+
+Return new lask_worked_mgroups string
+'''
+def schedule_message_sends(scheduler, usersettingsarr):
+    
+
+
+    return new_last_work_mgroups
+    pass
